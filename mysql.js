77666a93f0d5e15de293021abc1832a1dee8360a
Agilan <agilan20042003@gmail.com>
2022-11-17 01:25:34 +0530

    db changes

--- mysql.js
+++ mysql.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql");
 
 conn = mysql.createConnection({
-    // host: 'mysql5030.site4now.net',
-    // user: 'a8fe6b_agiagi1',
-    // password: 'Agilan@2003',
-    // database: 'db_a8fe6b_agiagi1'
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "vcdb",
-    port:3306
+    host: 'mysql5030.site4now.net',
+    user: 'a8fe6b_agiagi1',
+    password: 'Agilan@2003',
+    database: 'db_a8fe6b_agiagi1'
+    // host: "localhost",
+    // user: "root",
+    // password: "password",
+    // database: "vcdb",
+    // port:3306
 });
 
 conn.connect((err) => {
